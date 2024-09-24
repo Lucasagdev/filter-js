be18c3f6166600ed5823fce55b8847f84e897843
Lucas Gomes <lucasagostini400@gmail.com>
2024-09-23 23:36:56 -0300

    Usando filter em arrays no Javascript

---
+++ index.js
@@ -0,0 +1,24 @@
+let pessoas = [
+    { nome: 'Roberto', idade: 33 },
+    { nome: 'Ricardo', idade: 33 },
+    { nome: 'Rafhael', idade: 27}
+]
+
+// let pessoasComIdadeDe33Anos = []
+// for(let i = 0; i < pessoas.length; i++){
+//     if(pessoas[i].idade === 33){
+//         pessoasComIdadeDe33Anos.push(pessoas[i])
+//     }
+// }
+
+// console.log(pessoasComIdadeDe33Anos)
+
+let pessoasComIdadeDe33Anos = pessoas.filter(function(pessoa){
+    return pessoa.idade === 33
+})
+
+// ou usar um arrow function //
+
+// let pessoasComIdadeDe33Anos = pessoas.filter((pessoa) => pessoa.idade === 33)
+
+console.log(pessoasComIdadeDe33Anos)
